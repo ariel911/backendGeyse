773e23725d3a5579513f39ab02a49c501e55b8a3
Ariel Achu <achu.ariel.gabriel@gmail.com>
2024-08-26 00:55:48 -0400

    agregando usuario activo

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,7 +3,7 @@ const models = require("../database/models/index")
 const bcrypt = require('bcryptjs') // para encriptar la contraseña
 
 const signJWT = require('../middlewares/signJWT') // para crear el token
-const {encrypt}=require('../helpers/handleBcrypt')
+const { encrypt } = require('../helpers/handleBcrypt')
 
 module.exports = {
 
@@ -11,43 +11,43 @@ module.exports = {
         try {
             // 1. Verifico que el usuario exista solo comparando con el email
             const user = await models.usuario.findOne({
-
-                    include:[{
-                      model:models.rol,
-                      attributes:['id','nombre_rol']
-                    }],
-                  
-                
+                include: [{
+                    model: models.rol,
+                    attributes: ['id', 'nombre_rol']
+                }],
                 where: {
                     correo: req.body.correo
                 }
-            })
-            var contraseniaCoincide = false
+            });
+
+            var contraseniaCoincide = false;
             if (user) {  // Si existe el usuario
                 // 2. Verifico que la contraseña sea correcta
-                contraseniaCoincide = await bcrypt.compareSync(req.body.clave, user.clave) 
+                contraseniaCoincide = await bcrypt.compareSync(req.body.clave, user.clave);
                 // Comparo la contraseña ingresada con la contraseña de la base de datos
             }
-            if (!user || !contraseniaCoincide) {
+
+            // 3. Verifico que el usuario esté activo
+            if (!user || !contraseniaCoincide || user.estado !== 1) {
                 return res.status(500).json({
                     success: false,
                     error: 'usuario o contraseña incorrecta'
-                  });
+                });
             }
 
-
-            // 3. Si todo está bien, devuelvo el token
+            // 4. Si todo está bien, devuelvo el token
             res.json({
                 success: true,
                 data: {
                     token: signJWT(user), // Creo el token con los datos del usuario
-                    usuario:user
+                    usuario: user
                 }
-            })
+            });
         } catch (err) {
-            return next(err)
+            return next(err);
         }
-    },
+    }
+    ,
     acces: async (req, res, next) => {
         try {
             // 1. Verifico que el usuario exista solo comparando con el email
@@ -59,14 +59,14 @@ module.exports = {
             var contraseniaCoincide = false
             if (user) {  // Si existe el usuario
                 // 2. Verifico que la contraseña sea correcta
-                contraseniaCoincide = await bcrypt.compareSync(req.body.clave, user.clave) 
+                contraseniaCoincide = await bcrypt.compareSync(req.body.clave, user.clave)
                 // Comparo la contraseña ingresada con la contraseña de la base de datos
             }
             if (!user || !contraseniaCoincide) {
                 return res.status(500).json({
                     success: false,
                     error: 'usuario o contraseña incorrecta'
-                  });
+                });
             }
             res.json({
                 success: true,
@@ -83,29 +83,29 @@ module.exports = {
         }
     },
     registrarse: async (req, res, next) => {
-     
-            
-            const claveHash= await encrypt(req.body.clave)
-            const registerUser= await models.usuario.create({
-                correo:req.body.correo,
-                apellido:req.body.apellido,
-                nombre:req.body.nombre,
-                clave:claveHash,
-                ci:req.body.ci,
-                telefono:req.body.telefono,
-                direccion:req.body.direccion,
-                fecha_registro:req.body.fecha_registro,
-                
-            })
-            const relacion = await models.usuario_roles.create({
-                usuarioId: registerUser.id,
-                rolId: req.body.rolId
-                
-              })
-            
-            res.send({data:registerUser})
 
-        
+
+        const claveHash = await encrypt(req.body.clave)
+        const registerUser = await models.usuario.create({
+            correo: req.body.correo,
+            apellido: req.body.apellido,
+            nombre: req.body.nombre,
+            clave: claveHash,
+            ci: req.body.ci,
+            telefono: req.body.telefono,
+            direccion: req.body.direccion,
+            fecha_registro: req.body.fecha_registro,
+
+        })
+        const relacion = await models.usuario_roles.create({
+            usuarioId: registerUser.id,
+            rolId: req.body.rolId
+
+        })
+
+        res.send({ data: registerUser })
+
+
     }
 
 }
