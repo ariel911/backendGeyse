de0b8489efb1b5b73be330ccd3513fa3f746bfe9
Ariel Achu <achu.ariel.gabriel@gmail.com>
2024-09-18 01:54:43 -0400

    re

--- api/database/config/config.js
+++ api/database/config/config.js
@@ -24,7 +24,4 @@ module.exports = {
   }
   
 };
-console.log("DB_USERNAME:", process.env.DB_USERNAME);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_NAME:", process.env.DB_NAME);
+
